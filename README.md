0726be4fddbe92379c766c51fea7560b1e121d2a
nathsou <nathsoufflet@gmail.com>
2020-09-14 03:46:31 +0200

    Programs can be compiled to native binaries directly using ocamlc

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-### Croco
+# Croco
 
-A basic purely functional programming language which compiles down to 
+A basic purely functional programming language which compiles down to
 [girafe](https://github.com/nathsou/Girafe/).
 
-### Usage
+## Usage
 
 ```bash
 $ croco source.cro [out] [js/haskell/ocaml/girafe]
@@ -11,8 +11,6 @@ $ croco source.cro [out] [js/haskell/ocaml/girafe]
 
 ### Todo
 
-- [ ] allow lowercase function names
-- [x] add lambda expressions
-- [x] add let .. in expressions
-- [x] support function application
-- [ ] support IO actions using monads
+- [ ] support importing girafe files
+- [ ] support laziness
+- [ ] IO
--- cli/croco.ts
+++ cli/croco.ts
@@ -1,57 +1,120 @@
-import { writeFileSync } from "fs";
-import { arithmeticExternals, fun, makeNat, mergeExternals, metaExternals, nodeWorkerNormalizer, showTRS, supportedTargets, Targets, translate, TRS } from "girafe";
+import { spawn } from "child_process";
+import { unlinkSync, writeFileSync } from "fs";
+import { arithmeticExternals, fun, makeNat, mergeExternals, metaExternals, nodeWorkerNormalizer, OCamlTranslator, showTRS, supportedTargets, Targets, translate, TRS } from "girafe";
 import { compile } from "../src/Compiler/Compiler";
 import { postprocessTerm } from "../src/Parser/Expr";
 import { parse } from "../src/Parser/Parser";
 
-const [src, out, target] = process.argv.slice(2);
+const OCAMLC = 'ocamlc';
 
 const printUsage = () => {
-    console.info('usage: croco src.cro [out] [js/haskell/ocaml/girafe]');
+    console.info('The inglorious Croco compiler - github.com/nathsou/Croco');
+    console.info('---------------------------------------------------------');
+    console.info('interpret: croco src.cro [out]');
+    console.info('compile to intermediate language: croco src.cro [out] [js/haskell/ocaml/girafe]');
+    console.info('compile to binary: croco src.cro -c out');
     process.exit(0);
 };
+
+const externals = mergeExternals(arithmeticExternals, metaExternals());
+
+const src = process.argv[2];
 
 if (src === undefined) {
     printUsage();
 }
 
-const targets = [...supportedTargets, 'girafe'];
-
-const externals = mergeExternals(arithmeticExternals, metaExternals());
-
 const trs = compile(parse(src), externals);
 
-const transpile = (trs: TRS, target: Targets | 'girafe') => {
-    if (target === 'girafe') {
-        return showTRS(trs);
-    }
+if (process.argv[3] === '-c') {
+    const out = process.argv[4];
 
-    return translate(trs, target, externals);
-};
+    const ocamlt = new OCamlTranslator(trs, externals('ocaml'));
 
-if (out) {
-    if (target) {
-        if (targets.includes(target)) {
-            writeFileSync(out, transpile(trs, target as Targets | 'girafe'));
+    const runOcamlc = (srcFile: string): Promise<void> => {
+        return new Promise<void>((resolve, reject) => {
+            const instance = spawn(OCAMLC, [srcFile, '-o', out]);
+
+            let stderr = '';
+
+            instance.stdout.on('data', (data: Buffer) => {
+                console.info('ocamlc: ', data);
+            });
+
+            instance.stderr.on('data', (data: Buffer) => {
+                stderr += data.toString('utf-8');
+            });
+
+            instance.on('close', () => {
+                // remove the temporary file
+                unlinkSync(`${out}.ml`);
+                unlinkSync(`${out}.cmi`);
+                unlinkSync(`${out}.cmo`);
+
+                if (stderr !== '') {
+                    reject(stderr);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    };
+
+    const sourceWithQuery = () => {
+        return [
+            ocamlt.translate(),
+            'in print_endline (show_term (grf_Main ()));;'
+        ].join('\n');
+    };
+
+    const run = async () => {
+        writeFileSync(`${out}.ml`, sourceWithQuery());
+        try {
+            await runOcamlc(`${out}.ml`);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    run();
+} else {
+    const [out, target] = process.argv.slice(3);
+
+    const targets = [...supportedTargets, 'girafe'];
+
+    const transpile = (trs: TRS, target: Targets | 'girafe') => {
+        if (target === 'girafe') {
+            return showTRS(trs);
+        }
+
+        return translate(trs, target, externals);
+    };
+
+    if (out) {
+        if (target) {
+            if (targets.includes(target)) {
+                writeFileSync(out, transpile(trs, target as Targets | 'girafe'));
+            } else {
+                console.log(`invalid target: ${target}, options are: ${targets.join(', ')}`);
+                process.exit();
+            }
         } else {
-            console.log(`invalid target: ${target}, options are: ${targets.join(', ')}`);
-            process.exit();
+            if (targets.includes(out as Targets | 'girafe')) {
+                console.log(transpile(trs, out as Targets | 'girafe'));
+            } else {
+                printUsage();
+            }
         }
     } else {
-        if (targets.includes(out as Targets | 'girafe')) {
-            console.log(transpile(trs, out as Targets | 'girafe'));
-        } else {
-            printUsage();
+        // const norm = new DecisionTreeNormalizer(trs).asNormalizer(externals('native'));
+        // console.log(postprocessTerm(norm(fun('Main'))));
+
+        if (trs.has('Main')) {
+            const normalize = nodeWorkerNormalizer(trs, externals('js'), makeNat);
+            normalize(fun('Main')).then(out => {
+                console.log(postprocessTerm(out));
+            });
         }
     }
-} else {
-    // const norm = new DecisionTreeNormalizer(trs).asNormalizer(externals('native'));
-    // console.log(postprocessTerm(norm(fun('Main'))));
 
-    if (trs.has('Main')) {
-        const normalize = nodeWorkerNormalizer(trs, externals('js'), makeNat);
-        normalize(fun('Main')).then(out => {
-            console.log(postprocessTerm(out));
-        });
-    }
 }
